b80e14734e3685fc80673d305cc1c4bfd1dbd054
Diyar <diyarfranklin2000@gmail.com>
2023-12-04 14:16:45 +0100

    added files

--- klachtsysteem/views.py
+++ klachtsysteem/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from django.views.generic import TemplateView
+from django.views import View
 
 # Create your views here.
+class ComplaintsFormView(TemplateView):
+    template_name = "klachtenformulier.html"
+
+class HomePageView(TemplateView):
+    template_name = "homepage.html"

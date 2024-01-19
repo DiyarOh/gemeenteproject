d7d1a6705032ab87aa8a6de1f242539a53a482c7
CaptainSpazmo <mikeyheinrici@gmail.com>
2024-01-19 21:42:20 +0100

    GPS beta test
    first beta commit

--- klachtsysteem/models.py
+++ klachtsysteem/models.py
@@ -1,18 +1,23 @@
+# models.py
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 from django.utils.crypto import get_random_string
 from django.contrib.gis.db import models as geomodels
 
-
+# Custom manager for Invitation model
 class InvitationManager(models.Manager):
     def create_random_invitation(self):
+        # Generate a random code for a new invitation
         code = get_random_string(length=20)
         return self.create(code=code)
 
+# Invitation model
 class Invitation(models.Model):
     code = models.CharField(max_length=20, unique=True, blank=True, null=True)
     is_used = models.BooleanField(default=False)
+
+    objects = InvitationManager()
 
     def save(self, *args, **kwargs):
         # Generate a random unique code if not provided
@@ -23,12 +28,13 @@ class Invitation(models.Model):
     def __str__(self):
         return self.code if self.code else 'No Code'
 
-
+# Status model
 class Status(models.Model):
-    waarde = models.CharField(max_length=255)  # Value of the status
+    waarde = models.CharField(max_length=255, null=False)  # Status value
     beschrijving = models.TextField()
 
-    def seed_status_data():
+    @classmethod
+    def seed_status_data(cls):
         # Define a list of basic statuses
         status_data = [
             {'waarde': 'Open', 'beschrijving': 'The complaint is open and not yet resolved.'},
@@ -38,7 +44,7 @@ class Status(models.Model):
 
         # Create status objects if they don't already exist
         for status_info in status_data:
-            status, created = Status.objects.get_or_create(
+            status, created = cls.objects.get_or_create(
                 waarde=status_info['waarde'],
                 beschrijving=status_info['beschrijving']
             )
@@ -46,24 +52,19 @@ class Status(models.Model):
             if created:
                 print(f"Created status: {status}")
 
-    if __name__ == "__main__":
-        seed_status_data()
-
     def __str__(self):
         return self.waarde
 
-
+# Klacht model
 class Klacht(models.Model):
     naam = models.CharField(max_length=50, null=False)
     omschrijving = models.CharField(max_length=50, null=False)
     email = models.EmailField(null=False)
     GPS_locatie = geomodels.PointField()  # GeoDjango specific field for storing geographic location
     datum_melding = models.DateTimeField(null=False)
-    status = models.ForeignKey(Status, on_delete=models.PROTECT, default=1)  
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, default=1) 
 
-
+# Afbeelding model
 class Afbeelding(models.Model):
     klacht = models.ForeignKey(Klacht, related_name='afbeeldingen', on_delete=models.CASCADE)
-    image_file = models.ImageField(upload_to='afbeeldingen/')  
-
-
+    image_file = models.ImageField(upload_to='afbeeldingen/')
--- klachtsysteem/urls.py
+++ klachtsysteem/urls.py
@@ -1,8 +1,10 @@
+# urls.py
 from django.urls import path
-from .views import ComplaintsFormView, ComplaintsDashboard
-
+from .views import ComplaintsFormView, ComplaintsDashboard, submit_klacht
 
 urlpatterns = [
-    path('klachtenformulier/', ComplaintsFormView.as_view(), name='complaints_form'),
+    path('klachtformulier/', ComplaintsFormView.as_view(), name='complaints_form'),
     path('dashboard/', ComplaintsDashboard.as_view(), name='complaints_dashboard'),
+    path('submit_klacht/', submit_klacht, name='submit_klacht'),  # Added line for submit_klacht
+    # If you have other URL patterns, add them here
 ]
